2437050c374d64b585759570de1eecd08eab374e
Prabu Arjunan <31085459+prabuarjunan@users.noreply.github.com>
2023-04-27 10:22:59 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "netapp-gcp_storage_pool" "test-storage-pool" {
+  name                      = var.storage_pool_name
+  region                    = var.region
+  zone                      = var.zone
+  secondary_zone            = var.storage_pool_secondary_zone == "" ? null : var.storage_pool_secondary_zone
+  network                   = var.network_name
+  global_ad_access          = var.storage_pool_global_ad_access
+  size                      = var.storage_pool_size
+  service_level             = var.storage_pool_service_level
+  storage_class             = var.storage_pool_storage_class
+  shared_vpc_project_number = var.shared_vpc_project_number == "" ? null : var.shared_vpc_project_number
+  billing_label {
+    key   = var.storage_pool_billing_label_key
+    value = var.storage_pool_billing_label_value
+  }
+}
